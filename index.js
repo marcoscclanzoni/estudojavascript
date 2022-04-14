f57ea7109b9125b1e3ebae6fae63a9b8cb6d6645
Marcos Lanzoni <marcoslanzoni@gmail.com>
2022-04-14 19:58:55 -0300

    primeiro programa

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-console.log("Ola Nundo") 
-// primeiro limha de programa em node js
+var texto = " Alo Mundo" //criado uma variavel com nome texto e nela esta contido o valor " Alo Mundo "
+function imprimirtexto (textoParaImprimir){
+    console.log(textoParaImprimir)
+}
+//acima criado um função com nome "imprimir texto" e entre parenteses é passado um parametro com nome "textoParaImprimir"
+// em console.log é o codigo  de impressão 
+imprimirtexto(texto) // aqui é feita a chamada da função
